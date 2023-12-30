0f1b796d0c9ac433330d6a52782ca0ca5d5a27b0
emmecappa <117775229+emmecappa@users.noreply.github.com>
2023-12-30 13:44:52 +0000

    remove console log

--- test/app/lib/utils.js
+++ test/app/lib/utils.js
@@ -2,8 +2,6 @@ import emailjs from '@emailjs/browser';
 
 
 export function sendEamilUtil(templateParams){
-    console.log('sono qui bella');
-    console.log('process.env.EMAILJS_PUBBLIC_KEY)',process.env.NEXT_PUBLIC_EMAILJS_PUBBLIC_KEY);
     emailjs.send(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, templateParams, process.env.NEXT_PUBLIC_EMAILJS_PUBBLIC_KEY)
     .then((result) => {
       return true;
