4315fd3183833e203c6ac449ae3469974a480d62
Davide J. Spiga <92852593+TheJhonni@users.noreply.github.com>
2024-04-08 09:04:51 +0000

    commit

---
+++ app/[lang]/components/footer/footer.js
@@ -0,0 +1,17 @@
+// import Socials from "../socials";
+
+// const Footer = () => {
+//     return (
+//     <>
+//       <div className="container mx-auto">
+//         <div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
+//           <div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
+//             <Socials/>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//     );
+//   };
+  
+//   export default Footer;
