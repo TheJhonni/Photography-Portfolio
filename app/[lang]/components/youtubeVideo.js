1ecbc610851f1c2a97ad4713eef9885baa440214
Davide J. Spiga <92852593+TheJhonni@users.noreply.github.com>
2024-04-15 08:23:20 +0000

    key on parent suspance

--- app/[lang]/components/youtubeVideo.js
+++ app/[lang]/components/youtubeVideo.js
@@ -4,13 +4,6 @@ import React, { Suspense } from "react";
 import YouTube from "react-youtube"; 
 
 const YoutubeVideo = ({ data }) => {
-  // const opts = { 
-  //   height: "390", 
-  //   width: "100%", 
-  //   playerVars: { 
-  //     autoplay: 1, 
-  //   },
-  // };
   const pauseVideo = (event) => { 
     event?.target.pauseVideo(); 
   } 
@@ -20,8 +13,8 @@ const YoutubeVideo = ({ data }) => {
     <>
           {videoData && videoData.map((videoId, index) => {
     return ( 
-      <Suspense fallback={<p>Loading videos...</p>}>
-        <div key={index} className={`w-full lg:w-[800px] xl:w-[1200px] mx-auto ${index === 0 ? 'pb-10' : ''}`}>
+      <Suspense key={index} fallback={<p>Loading videos...</p>}>
+        <div className={`w-full lg:w-[800px] xl:w-[1200px] mx-auto ${index === 0 ? 'pb-10' : ''}`}>
           <YouTube videoId={videoId}
             opts={{
               height: "390",
