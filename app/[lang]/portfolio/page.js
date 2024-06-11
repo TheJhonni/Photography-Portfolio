84ff03de62a1e73942040b0b031c285df7cd7b8c
Davide J. Spiga <92852593+TheJhonni@users.noreply.github.com>
2024-06-11 20:49:36 +0000

    delete pb-44

--- app/[lang]/portfolio/page.js
+++ app/[lang]/portfolio/page.js
@@ -16,7 +16,7 @@ async function Portfolio(){
       <div className='container mx-auto h-full relative text-orange'>
         <div className='flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:pl-[30px] pt-20 md:pt-80 lg:pt-0'>
             {data &&
-                <div className='flex flex-col pb-44 lg:items-center'>
+                <div className='flex flex-col lg:items-center'>
                   <div className='px-6 lg:p-0 lg:mb-6'>
                     <Paragraph data={data} /> 
                   </div>
